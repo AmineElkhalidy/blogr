ad3026398bcc29e193d0247be4ce209bc4ff40a9
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-11-26 14:44:07 +0100

    Navbar added!

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,9 +13,12 @@ import {
 
 const Header: React.FC = () => {
   const [active, setActive] = useState(false);
+  const [changeArrowOne, setChangeArrowOne] = useState(false);
+  const [changeArrowTwo, setChangeArrowTwo] = useState(false);
+  const [changeArrowThree, setChangeArrowThree] = useState(false);
 
   return (
-    <header className="relative w-full h-full bg-lighterRed from-r to-lightRed">
+    <header className="relative w-full h-full bg-gradient-to-r from-lightRed to-lighterRed">
       <nav className="h-16 flex justify-between items-center px-6">
         <img
           className="h-6"
@@ -23,53 +26,87 @@ const Header: React.FC = () => {
           alt="Blogr navbar logo"
           loading="lazy"
         />
-
         {/* Mobile menu */}
-        {active ? (
-          <div className="flex justify-center items-center h-full w-full px-6">
-            <div className="absolute top-20 left-0 w-full bg-white shadow-md">
-              {/* Navigation Links */}
-              <ul className="flex flex-col items-center justify-center gap-6 border-b-2/80">
-                <li>
-                  <a className="inline-flex items-center gap-1" href="#product">
-                    Product{" "}
-                    <span>
-                      <ChevronDownIcon className="w-4 h-4" />
-                    </span>
-                  </a>
-                </li>
+        {active && (
+          <div className="absolute top-20 left-0 w-full">
+            <div className="px-6">
+              <div className="bg-white rounded-md p-8">
+                {/* Navigation Links */}
+                <ul className="flex flex-col items-center justify-center gap-6 border-b-2 py-4">
+                  <li>
+                    <a
+                      className="inline-flex items-center gap-1 text-darkBlue font-semibold"
+                      href="#product"
+                    >
+                      Product{" "}
+                      {!changeArrowOne ? (
+                        <span onClick={() => setChangeArrowOne(true)}>
+                          <ChevronDownIcon className="w-4 h-4 text-lightRed" />
+                        </span>
+                      ) : (
+                        <span onClick={() => setChangeArrowOne(false)}>
+                          <ChevronUpIcon className="w-4 h-4 text-lightRed" />
+                        </span>
+                      )}
+                    </a>
+                  </li>
 
-                <li>
-                  <a className="inline-flex items-center gap-1" href="#company">
-                    Company{" "}
-                    <span>
-                      <ChevronDownIcon className="w-4 h-4" />
-                    </span>
-                  </a>
-                </li>
+                  <li>
+                    <a
+                      className="inline-flex items-center gap-1 text-darkBlue font-semibold"
+                      href="#company"
+                    >
+                      Company{" "}
+                      {!changeArrowTwo ? (
+                        <span onClick={() => setChangeArrowTwo(true)}>
+                          <ChevronDownIcon className="w-4 h-4 text-lightRed" />
+                        </span>
+                      ) : (
+                        <span onClick={() => setChangeArrowTwo(false)}>
+                          <ChevronUpIcon className="w-4 h-4 text-lightRed" />
+                        </span>
+                      )}
+                    </a>
+                  </li>
 
-                <li>
-                  <a className="inline-flex items-center gap-1" href="#connect">
-                    Connect{" "}
-                    <span>
-                      <ChevronDownIcon className="w-4 h-4" />
-                    </span>
-                  </a>
-                </li>
-              </ul>
+                  <li>
+                    <a
+                      className="inline-flex items-center gap-1 text-darkBlue font-semibold"
+                      href="#connect"
+                    >
+                      Connect{" "}
+                      {!changeArrowThree ? (
+                        <span onClick={() => setChangeArrowThree(true)}>
+                          <ChevronDownIcon className="w-4 h-4 text-lightRed" />
+                        </span>
+                      ) : (
+                        <span onClick={() => setChangeArrowThree(false)}>
+                          <ChevronUpIcon className="w-4 h-4 text-lightRed" />
+                        </span>
+                      )}
+                    </a>
+                  </li>
+                </ul>
 
-              {/* Login & Sign Up buttons container */}
-              <div className="flex flex-col justify-center items-center gap-4">
-                <a className="" href="#login">
-                  Login
-                </a>
-                <a className="" href="#sign-up">
-                  Sign Up
-                </a>
+                {/* Login & Sign Up buttons container */}
+                <div className="flex flex-col justify-center items-center gap-4 mt-6">
+                  <a className="text-darkBlue font-semibold" href="#login">
+                    Login
+                  </a>
+                  <a
+                    className="text-white font-semibold py-3 px-8 rounded-full bg-gradient-to-l from-bg-lightRed to-bg-lighterRed/30"
+                    href="#sign-up"
+                  >
+                    Sign Up
+                  </a>
+                </div>
               </div>
             </div>
           </div>
-        ) : active ? (
+        )}
+
+        {/* Toggle nav icon */}
+        {active ? (
           <span
             onClick={() => setActive(false)}
             className="inline-flex items-center justify-center"
@@ -84,7 +121,6 @@ const Header: React.FC = () => {
             <Bars3Icon className="h-7 w-7 cursor-pointer text-white" />
           </span>
         )}
-
         {/* Desktop menu */}
         <div className="hidden">
           {/* Navigation Links */}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home: React.FC = () => {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <React.Fragment>
+      <section className="min-h-screen bg-gradient-to-r from-lightRed to-lighterRed"></section>
+    </React.Fragment>
+  );
 };
 
 export default Home;

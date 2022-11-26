72112fe7bab9dd99140e783751691c18f3b53781
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-11-26 12:30:20 +0100

    Necessary files and icons, styles, colors, images added!

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return <header className=""></header>;
+};
+
+export default Home;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: "Overpass, sans-serif",
+    },
+    extend: {
+      colors: {
+        lightRed: "hsl(356, 100%, 66%)",
+        lighterRed: "hsl(355, 100%, 74%)",
+        darkBlue: "hsl(208, 49%, 24%)",
+        grayishBlue: "hsl(240, 2%, 79%)",
+        darkGrayishBue: "hsl(207, 13%, 34%)",
+        darkBlackBlue: "hsl(240, 10%, 16%)",
+      },
+    },
   },
   plugins: [],
 };

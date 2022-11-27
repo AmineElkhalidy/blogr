fba9b25fd418d1aae64ef05651c997e60d9a4622
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-11-27 12:06:34 +0100

    Footer added

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,66 +5,94 @@ import Logo from "../../assets/images/logo.svg";
 
 const Footer = () => {
   return (
-    <footer className="max-w-6xl mx-auto">
-      <div>
-        {/* Footer logo */}
-        <img src={Logo} alt="Footer Logo" />
+    <footer className="bg-darkBlackBlue rounded-tr-[6rem] md:rounded-tr-[10rem]">
+      <div className="max-w-6xl mx-auto">
+        <div className="py-20 h-full flex justify-center items-center">
+          <div className="grid sm:grid-cols-4 sm:gap-14 md:gap-28 lg:gap-48">
+            {/* Footer logo */}
+            <img className="mb-14 sm:mb-0" src={Logo} alt="Footer Logo" />
 
-        {/* Product */}
-        <div>
-          <h3>Product</h3>
-          <ul>
-            <li>
-              <a href="#">Overview</a>
-            </li>
-            <li>
-              <a href="#">Pricing</a>
-            </li>
-            <li>
-              <a href="#">Marketplace</a>
-            </li>
-            <li>
-              <a href="#">Features</a>
-            </li>
-            <li>
-              <a href="#">Integrations</a>
-            </li>
-          </ul>
-        </div>
+            {/* Product */}
+            <div className="text-center mb-12 sm:text-left sm:mb-0">
+              <h3 className="text-xl font-semibold text-white mb-6">Product</h3>
+              <ul className="space-y-2">
+                <li>
+                  <a className="text-lg text-grayishBlue font-medium" href="#">
+                    Overview
+                  </a>
+                </li>
+                <li>
+                  <a className="text-lg text-grayishBlue font-medium" href="#">
+                    Pricing
+                  </a>
+                </li>
+                <li>
+                  <a className="text-lg text-grayishBlue font-medium" href="#">
+                    Marketplace
+                  </a>
+                </li>
+                <li>
+                  <a className="text-lg text-grayishBlue font-medium" href="#">
+                    Features
+                  </a>
+                </li>
+                <li>
+                  <a className="text-lg text-grayishBlue font-medium" href="#">
+                    Integrations
+                  </a>
+                </li>
+              </ul>
+            </div>
 
-        {/* Company */}
-        <div>
-          <h3>Company</h3>
-          <ul>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Team</a>
-            </li>
-            <li>
-              <a href="#">Blog</a>
-            </li>
-            <li>
-              <a href="#">Careers</a>
-            </li>
-          </ul>
-        </div>
+            {/* Company */}
+            <div className="text-center mb-12 sm:text-left sm:mb-0">
+              <h3 className="text-xl font-semibold text-white mb-6">Company</h3>
+              <ul className="space-y-2">
+                <li>
+                  <a className="text-lg text-grayishBlue font-medium" href="#">
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a className="text-lg text-grayishBlue font-medium" href="#">
+                    Team
+                  </a>
+                </li>
+                <li>
+                  <a className="text-lg text-grayishBlue font-medium" href="#">
+                    Blog
+                  </a>
+                </li>
+                <li>
+                  <a className="text-lg text-grayishBlue font-medium" href="#">
+                    Careers
+                  </a>
+                </li>
+              </ul>
+            </div>
 
-        {/* Connect */}
-        <div>
-          <h3>Connect</h3>
-          <ul>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-            <li>
-              <a href="#">Newsletter</a>
-            </li>
-            <li>
-              <a href="#">LinkedIn</a>
-            </li>
-          </ul>
+            {/* Connect */}
+            <div className="text-center sm:text-left">
+              <h3 className="text-xl font-semibold text-white mb-6">Connect</h3>
+              <ul className="space-y-2">
+                <li>
+                  <a className="text-lg text-grayishBlue font-medium" href="#">
+                    Contact
+                  </a>
+                </li>
+                <li>
+                  <a className="text-lg text-grayishBlue font-medium" href="#">
+                    Newsletter
+                  </a>
+                </li>
+                <li>
+                  <a className="text-lg text-grayishBlue font-medium" href="#">
+                    LinkedIn
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </footer>

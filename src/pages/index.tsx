d8a055fc242c1b13705065ff075e58a67eb992a1
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-11-28 12:55:55 +0100

    Fixing sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -94,26 +94,32 @@ const Home: React.FC = () => {
         <div className="grid md:grid-cols-2 space-y-4">
           {/* Image */}
           <div className="relative">
-            <img className="" src={Circle} alt="Circle" />
+            <img
+              className="sm:w-[80%] md:w-[70%] lg:w-[60%]"
+              src={Circle}
+              alt="Circle"
+            />
 
             <img
-              className="absolute -top-16"
+              className="absolute -top-16 sm:w-[80%]"
               src={Phones}
               alt="Phones circled"
             />
           </div>
 
           {/* Art Data */}
-          <div className="text-center space-y-6 py-6">
-            <h2 className="text-3xl font-semibold text-white">
-              State of the Art Infrastructure
-            </h2>
-            <p className="text-gray-200">
-              With reliability and speed in mind, worldwide data centers provide
-              the backbone for ultra-fast connectivity. This ensures your site
-              will load instantly, no matter where your readers are, keeping
-              your site competitive.
-            </p>
+          <div className="text-center space-y-6 p-6 grid md:items-center md:text-left">
+            <div>
+              <h2 className="text-3xl font-semibold text-white">
+                State of the Art Infrastructure
+              </h2>
+              <p className="text-gray-200">
+                With reliability and speed in mind, worldwide data centers
+                provide the backbone for ultra-fast connectivity. This ensures
+                your site will load instantly, no matter where your readers are,
+                keeping your site competitive.
+              </p>
+            </div>
           </div>
         </div>
       </section>

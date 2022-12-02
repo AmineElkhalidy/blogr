6bde05649a42f189ef61d831aeded04d5d9106c0
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2022-12-02 21:43:00 +0100

    Chakra UI added with theme

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,24 @@ import "./index.css";
 // Chakra provider
 import { ChakraProvider } from "@chakra-ui/react";
 
+// Extend theme function
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+  lightRed: "hsl(356, 100%, 66%)",
+  lighterRed: "hsl(355, 100%, 74%)",
+  darkBlue: "hsl(208, 49%, 24%)",
+  grayishBlue: "hsl(240, 2%, 79%)",
+  darkGrayishBue: "hsl(207, 13%, 34%)",
+  darkBlackBlue: "hsl(240, 10%, 16%)",
+};
+
+// Theme
+const theme = extendTheme({ colors });
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
